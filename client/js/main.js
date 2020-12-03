48c8422080eca4fd0370f1c8bd84dad27ca5de6a
fccJashCoda <personalrevolution2014@gmail.com>
2020-12-03 21:59:58 +0100

    correctly display bar chart along the x axis

--- client/js/main.js
+++ client/js/main.js
@@ -2,16 +2,43 @@ document.addEventListener('DOMContentLoaded', () => {
   const url =
     // 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
     'http://localhost:5555/jsondata';
-  const width = 500;
-  const height = 400;
+  const width = 1000;
+  const height = 500;
 
   fetch(url)
     .then((response) => response.json())
     .then(({ data }) => {
-      d3.select('article')
+      console.log(data);
+      const dataset = data.data;
+
+      const xScale = d3
+        .scaleBand()
+        .domain(d3.range(dataset.length))
+        .range([0, width])
+        .padding(0.1);
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, d3.max(dataset, (d) => d[1])])
+        .range([height, 0]);
+
+      const svg = d3
+        .select('article')
         .append('svg')
         .attr('width', width)
         .attr('height', height);
+
+      svg
+        .selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+        .attr('x', (d, i) => xScale(i))
+        .attr('y', (d) => height - 30 - d[1] / 45)
+        .attr('width', 3)
+        // .attr('width', d => xScale(d[1]))
+        .attr('height', (d) => d[1] / 45)
+        .attr('fill', 'black')
+        .attr('class', 'bar');
     })
     .catch((err) => console.error(err));
 });
