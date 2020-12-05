1d78eda108e4930208e3ef03c79794747d253ca6
fccJashCoda <personalrevolution2014@gmail.com>
2020-12-05 23:40:07 +0100

    added tooltips, all tests passing

--- client/js/main.js
+++ client/js/main.js
@@ -33,7 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
       data.data.forEach((set) => {
         const dataobj = {
-          // date: new Date(set[0]),
           date: set[0],
           year: set[0].slice(0, 4),
           quarter: getQuarter(set[0].slice(5)),
@@ -43,10 +42,14 @@ document.addEventListener('DOMContentLoaded', () => {
       });
 
       const testData = dataset.map((set) => new Date(set.date));
+      const exper = new Date(d3.max(testData).getTime());
+      exper.setFullYear(exper.getFullYear() + 1);
 
+      console.log('d3 max testData ', exper);
       const timeScale = d3
         .scaleTime()
-        .domain([d3.min(testData), new Date('2016')])
+        .domain([d3.min(testData), exper])
+        // .domain([d3.min(testData), new Date('2016')])
         .range([padding, width]);
 
       const xScale = d3
@@ -61,7 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const xAxis = d3.axisBottom(timeScale);
       const yAxis = d3.axisLeft(yScale);
-      console.log('xAxis:', xAxis);
 
       const svg = d3
         .select('article')
@@ -70,11 +72,14 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('width', width)
         .attr('height', height);
 
+      const tooltip = d3.select('article').append('div').attr('id', 'tooltip');
+
       svg
         .selectAll('rect')
         .data(dataset)
         .enter()
         .append('rect')
+        .style('position', 'relative')
         .attr('class', 'bar')
         .attr('x', (d, i) => timeScale(testData[i]))
         .attr('y', (d) => yScale(d.gdp))
@@ -83,8 +88,17 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('fill', 'steelblue')
         .attr('data-date', (d) => d.date)
         .attr('data-gdp', (d) => d.gdp)
-        .append('title')
-        .text((d) => `${d.year} ${d.quarter} \n$${d.gdp} Billion`);
+        .on('mouseover', (d) => {
+          tooltip
+            .html(`${d.year} ${d.quarter} <br> $${d.gdp} Billion`)
+            .style('left', `${d3.event.pageX - 180}px`)
+            .attr('data-date', d.date)
+            .attr('data-gdp', d.gdp)
+            .style('visibility', 'visible');
+        })
+        .on('mouseout', () => {
+          tooltip.style('visibility', 'hidden');
+        });
 
       svg
         .append('g')
@@ -100,3 +114,16 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .catch((err) => console.error(err));
 });
+
+const sugar = 'sugar';
+
+(() => {
+  let data;
+
+  window.addEventListener('DOMContentLoaded', async () => {
+    const response = await fetch('http://localhost:5555/jsondata');
+    const fly = await response.json();
+    data = 'noodle';
+  });
+  if (data) console.log(data);
+})();
